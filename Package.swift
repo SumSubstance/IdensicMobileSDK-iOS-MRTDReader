318a77b55bd31a38bbb8121599ba4931aa8c4db4
Sergey Kokunov <sergress@gmail.com>
2026-06-30 16:36:41 +0300

    Release 1.45.0

--- Package.swift
+++ Package.swift
@@ -3,12 +3,12 @@
 
 import PackageDescription
 
-let version = Version("1.44.0")
+let version = Version("1.45.0")
 
 let gitUrl = "https://maven.sumsub.com/repository/releases/IdensicMobileSDK-iOS/\(version)"
 
 enum checksums {
-    static let mrtd = "c7d92cc94ab1037034185f23bd3329e18453c949a19c95919b570fbb22c26867"
+    static let mrtd = "6b37f3beb9c874a661fd3d9c9093821856b7b2a24efee32761e66e1ccdeb2682"
 }
 
 let package = Package(
@@ -27,7 +27,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/SumSubstance/IdensicMobileSDK-iOS.git", .exact(version)),
         .package(url: "https://github.com/krzyzanowskim/OpenSSL", "3.1.5001" ..< "4.0.0"),
-        .package(url: "https://github.com/OVD-Kinegram-AG/emrtd-connector-sdk-ios", "2.10.0" ..< "2.11.0")
+        .package(url: "https://github.com/OVD-Kinegram-AG/emrtd-connector-sdk-ios", "2.13.2" ..< "3.0.0")
     ],
     targets: [
         .binaryTarget(
